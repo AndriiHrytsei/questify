1912827a1f936a521c04352ff4f9073037b9851a
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-09-21 20:37:12 +0300

    add styles to login and registration

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -3,8 +3,10 @@ import { Navigate } from "react-router-dom";
 import { selectIsLoggedIn } from "../redux/auth/selectors";
 import PropTypes from "prop-types";
 
+
 export const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,36 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { login, logout, register } from "./operations";
 
-
 const initialState = {
   user: {
     name: null,
     email: null,
   },
   isLoggedIn: false,
-  token: null
+  token: null,
 };
 
 const authSlice = createSlice({
-  name: 'auth',
-  initialState,
+  name: "auth",
+  initialState: initialState,
   extraReducers: {
     [register.fulfilled](state, action) {
       state.user = action.payload.user;
-      state.isLoggedIn = true;
-      state.token = action.payload.token
+      state.token = action.payload.token;
     },
-    [login.fulfilled](state, action){
-      state.user = action.payload.user;
+    [login.fulfilled](state, action) {
+      state.user = action.payload.userData;
+      state.token = action.payload.accessToken;
       state.isLoggedIn = true;
-      state.token = action.payload.token
     },
     [logout.fulfilled](state) {
-      state.user = {name: '', email: ''};
+      state.user = { name: "", email: "" };
       state.isLoggedIn = false;
-      state.token = null
-    }
-  }
-})
+      state.token = null;
+    },
+  },
+});
 
-export const authReducer = authSlice.reducer
+export const authReducer = authSlice.reducer;
