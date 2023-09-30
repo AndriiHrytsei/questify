cdd215f8df232da63c18940b9c73e950abf493cc
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-09-30 14:34:46 +0300

    add logout; add userName in appbar

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://questify-backend.goit.global";
+axios.defaults.baseURL = 'https://questify-backend.goit.global';
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -15,11 +15,7 @@ export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("/auth/register", {
-        email: credentials.email,
-        password: credentials.password    
-      });
-      setAuthHeader(res.data.token);
+      const res = await axios.post("/auth/register", credentials);
       return res.data;
     } catch (err) {
       return thunkAPI.rejectWithValue(err.message);
@@ -32,7 +28,14 @@ export const login = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const res = await axios.post("/auth/login", credentials);
-      setAuthHeader(res.data.token);
+      setAuthHeader(res.data.accessToken);
+      localStorage.setItem("sid", JSON.stringify(res.data.sid));
+      localStorage.setItem(
+        "refreshToken",
+        JSON.stringify(res.data.refreshToken)
+      );
+      localStorage.setItem("accessToken", JSON.stringify(res.data.accessToken));
+      localStorage.setItem("cards", JSON.stringify(res.data.userData.cards));
       return res.data;
     } catch (err) {
       return thunkAPI.rejectWithValue(err.message);
@@ -42,9 +45,35 @@ export const login = createAsyncThunk(
 
 export const logout = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
-    await axios.post("/auth/logout");
+    await axios.post('/auth/logout');
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem('cards')
+    localStorage.removeItem('userName')
     clearAuthHeader();
   } catch (err) {
     return thunkAPI.rejectWithValue(err.message);
   }
 });
+
+export const refreshUser = createAsyncThunk(
+  "auth/refreshUser",
+  async (_, thunkAPI) => {
+    const sid = JSON.parse(localStorage.getItem('sid'))
+    const refreshToken = JSON.parse(localStorage.getItem('refreshToken'))
+    setAuthHeader(refreshToken)
+    if (sid === null) {
+      return thunkAPI.rejectWithValue('Unable to fetch user')
+    }
+    try {
+      const res = await axios.post('/auth/refresh', { sid })
+      setAuthHeader(res.data.newAccessToken)
+      localStorage.setItem('refreshToken', JSON.stringify(res.data.newRefreshToken))
+      localStorage.setItem('accessToken', JSON.stringify(res.data.newAccessToken))
+      localStorage.setItem('sid', JSON.stringify(res.data.newSid))
+      return res.data
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.message)
+    }
+  }
+);
--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -4,3 +4,4 @@ export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 
 export const selectUserName = (state) => state.auth.userName;
  
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
