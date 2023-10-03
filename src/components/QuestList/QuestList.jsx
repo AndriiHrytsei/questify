e1c432cb3725ee0b0b5db3075f8cd994dfcde066
Danyl Brahan <danylbragan@gmail.com>
2023-10-03 19:23:15 +0200

    sdnfjdff

--- src/components/QuestList/QuestList.jsx
+++ src/components/QuestList/QuestList.jsx
@@ -1,14 +1,14 @@
 // import React from 'react'
 import QuestCard from '../QuestCard/QuestCard'
 import AddQuestBtn from '../AddQuestBtn/AddQuestBtn'
-// import { getCards } from '../../redux/quests/selectors'
-import { useDispatch } from 'react-redux'
+import { getCards } from '../../redux/quests/selectors'
+import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 // import css from './QuestList.css'    
 import { fetchCards } from '../../redux/quests/operations'
 
 export default function QuestList() {
-  // const filteredCards = useSelector(getCards);
+  const cards = useSelector(getCards);
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(fetchCards())
