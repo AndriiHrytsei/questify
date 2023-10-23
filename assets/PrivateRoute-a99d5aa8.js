bfc1d4b89cabdc391fd165f1d4cf34614b7c20ff
AndriiHrytsei <AndriiHrytsei@users.noreply.github.com>
2023-10-23 18:50:29 +0000

    deploy: b2179a2140e813362f03bf348e193f3fdd132e50

--- assets/PrivateRoute-a99d5aa8.js
+++
@@ -1,1 +0,0 @@
-import{b as t,s as r,j as i,N as n}from"./index-c3e7c70e.js";import{P as e}from"./index-0d9231aa.js";const d=({component:s,redirectTo:o="/"})=>!t(r)?i.jsx(n,{to:o}):s;d.propTypes={component:e.element.isRequired,redirectTo:e.string.isRequired};export{d as default};
---
+++ assets/PrivateRoute-b892eba9.js
@@ -0,0 +1,1 @@
+import{b as t,s as r,j as i,N as n}from"./index-a741b2db.js";import{P as e}from"./index-df79cced.js";const d=({component:s,redirectTo:o="/"})=>!t(r)?i.jsx(n,{to:o}):s;d.propTypes={component:e.element.isRequired,redirectTo:e.string.isRequired};export{d as default};
