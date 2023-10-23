e350480cd4bde98a62588063127feab55f2bc264
AndriiHrytsei <AndriiHrytsei@users.noreply.github.com>
2023-10-23 14:52:38 +0000

    deploy: e8a0df1b7da91a8eb2e610379185637356278fa1

---
+++ assets/RestrictedRoute-052bb3ad.js
@@ -0,0 +1,1 @@
+import{b as t,s as r,j as i,N as n}from"./index-c3e7c70e.js";import{P as e}from"./index-0d9231aa.js";const p=({component:s,redirectTo:o="/"})=>t(r)?i.jsx(n,{to:o}):s;p.propTypes={component:e.element.isRequired,redirectTo:e.string.isRequired};export{p as default};
