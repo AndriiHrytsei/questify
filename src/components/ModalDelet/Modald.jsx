ca56252de855de39be0e5edc8ce7103f8095d3b2
Ваше Ім'я <karffffagen@gmail.com>
2023-10-24 17:27:19 +0300

    fafa

--- src/components/ModalDelet/Modald.jsx
+++ src/components/ModalDelet/Modald.jsx
@@ -1,26 +1,31 @@
 import { useDispatch } from "react-redux";
 import { deleteCard } from "../../redux/quests/operations";
 import { unsetIsCreating } from "../../redux/auth/authSlice";
-export default function Modald({card}) {
-    const dispatch = useDispatch();
-    
-    const handleDelete = () => {
-        dispatch(deleteCard(card._id));
-        dispatch(unsetIsCreating());
-      };
+import css from "./Modald.module.css";
+
+export default function Modald({card,onCancelClick}) {
+  const dispatch = useDispatch();
+  const handleDelete = () => {
+    dispatch(deleteCard(card._id));
+    dispatch(unsetIsCreating());
+  };
   return (
-    <>
-        <div>
-            <h3>Delete this Quest?</h3>
-            <ul>
-                <li>
-                    <button type="button">CANCEL</button>
-                </li>
-                <li>
-                    <button type='button' onClick={handleDelete}>DELETE</button>
-                </li>
-            </ul>
-        </div>
-    </>
-    )
+    <div className={css.hovers}>
+    <div className={css.bacroundTitle}>
+        <h3 className={css.text}>Delete This Quest?</h3>
+        <ul className={css.listButtons}>
+            <li>
+            <button className={css.buttonCancele} type="button" onClick={onCancelClick}>
+                CANCEL
+            </button>
+            </li>
+            <li>
+            <button type="button" onClick={handleDelete} className={css.buttonDelete}>
+                DELETE
+            </button>
+            </li>
+        </ul>
+    </div>
+    </div>
+  );
 }
